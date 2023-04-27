040735fd1ae014ebeed3147f3d2a67835b3ce052
Edgard <edigarhenriqu@hmail.com>
2023-04-27 19:06:22 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace estudo;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Informe um número para criar a tabuada:");
+       int num = int.Parse(Console.ReadLine());
+
+       int[] tabuada = new int[11];
+
+       for (int pas = 0; pas < tabuada.Length; pas++)
+       {
+            tabuada[pas] = num * pas;
+       }
+
+       Console.WriteLine($"A tabuada do {num} é:");
+       for (int pas = 0; pas < tabuada.Length; pas++)
+       {
+            Console.WriteLine($"{num} x {pas} = {tabuada[pas]}");
+       }
+    }
+
+
+}
